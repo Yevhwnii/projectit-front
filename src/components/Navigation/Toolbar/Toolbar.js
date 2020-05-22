621a535667c858384249aae5bf1272bbdb0fcd34
Yevhenii <mirandoos3223@gmail.com>
2020-05-22 19:03:17 +0200

    # Toolbar added (holds nav items)

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './Toolbar.module.css'
+import NavigationItems from '../NavigationItems/NavigationItems'
+
+const Toolbar = props => {
+    return (
+        <header className={classes.Toolbar}>
+            <div>
+                Logo
+            </div>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </header>
+    )
+};
+
+export default Toolbar;
