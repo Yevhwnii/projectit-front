3dbbe15b0386795724fd3209d3abd8d522c345f4
Yevhenii <mirandoos3223@gmail.com>
2020-05-22 18:50:28 +0200

    # Nav Item added

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+
+import classes from './NavigationItem.module.css'
+
+const NavItem = props => {
+    return (
+        <li className={classes.NavItem}>
+            <NavLink
+                to={props.link}
+                exact
+                activeClassName={classes.active}
+            >
+                {props.children}
+            </NavLink>
+        </li>
+    )
+};
+
+export default NavItem;
