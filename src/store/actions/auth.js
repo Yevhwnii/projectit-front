1660e7430f5de8f81f93ca9d3c55ee8611f27de2
Yevhwnii <mirandoos3223@gmail.com>
2020-06-14 16:46:51 +0200

    # Reducer fix

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,61 +1,59 @@
-import * as actionTypes from './actionTypes'
+import * as actionTypes from './actionTypes';
 
 // Auth process
 export const auth = (userId, password) => {
-    return {
-        type: actionTypes.AUTH_USER_BEGIN,
-        userId: userId,
-        password: password
-    }
-}
+  return {
+    type: actionTypes.AUTH_USER_BEGIN,
+    userId: userId,
+    password: password,
+  };
+};
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    }
-}
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
 
 export const authSuccess = (token, userId) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        token: token,
-        userId: userId
-    }
-}
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    token: token,
+    userId: userId,
+  };
+};
 
 export const authFail = (error) => {
-    console.log('I am in auth fail');
-    
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    }
-}
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
 ////
 
 // Auth check state
 export const checkAuthState = () => {
-    return {
-        type: actionTypes.AUTH_CHECK_STATE
-    }
-}
+  return {
+    type: actionTypes.AUTH_CHECK_STATE,
+  };
+};
 ////
 
 // Auth logout
 export const authLogout = () => {
-    return {
-        type: actionTypes.AUTH_LOGOUT_START
-    }
-}
+  return {
+    type: actionTypes.AUTH_LOGOUT_START,
+  };
+};
 export const authLogoutSucced = () => {
-    return {
-        type: actionTypes.AUTH_LOGOUT_SUCCED
-    }
-}
+  return {
+    type: actionTypes.AUTH_LOGOUT_SUCCED,
+  };
+};
 ////
 
 // Auth UI
 export const authRemoveError = () => {
-    return {
-        type: actionTypes.AUTH_REMOVE_ERROR
-    }
-}
+  return {
+    type: actionTypes.AUTH_REMOVE_ERROR,
+  };
+};
