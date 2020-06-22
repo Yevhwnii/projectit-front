f6fb3ef27be681cb2e9ac10eaa1692bb39b62526
Yevhwnii <mirandoos3223@gmail.com>
2020-06-22 17:15:20 +0200

    # Initial Billing container

---
+++ src/containers/Cashier/Billing/Billing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Billing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Billing system</h1>
+      </div>
+    );
+  }
+}
+export default Billing;
